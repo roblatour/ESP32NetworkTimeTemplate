e6d2b1aa238edd4c75b3e9dab703f8130c52a60b
Rob Latour <5200730+roblatour@users.noreply.github.com>
2025-01-17 18:20:03 -0500

    Update user_settings.h

--- ESP32NetworkTimeTemplate/user_settings.h
+++ ESP32NetworkTimeTemplate/user_settings.h
@@ -1,6 +1,6 @@
 #define USER_SETTINGS_NTP_SERVER_1                             "pool.ntp.org"
 #define USER_SETTINGS_NTP_SERVER_2                             "time.cloudflare.com"
-#define USER_SETTINGS_NTP_SERVER_2                             "time.nist.gov"
+#define USER_SETTINGS_NTP_SERVER_3                             "time.nist.gov"
 
 #define USER_SETTINGS_POSIX_TIME_ZONE_CODE                     "EST5EDT,M3.2.0,M11.1.0" // POSIX tme zone code for America/Toronto
                                                                 // Find your POSIX time zone code here: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.json
